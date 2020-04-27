8b148f6b932ff7d0d5c7f84ffe2c08a8fa40c5aa
lnlytumbleweed <legendary.js@gmail.com>
2020-04-27 16:07:42 +0200

    Folder Structure Edit

---
+++ vue-startpage/src/assets/MyBabylonScene.js
@@ -0,0 +1,8 @@
+import * as BABYLON from 'babylonjs'
+
+export class MyScene {
+
+  constructor(){ }
+
+  doAction(){ }
+}
---
+++ vue-startpage/src/components/babylon.vue
@@ -0,0 +1,19 @@
+<template>
+    <canvas id="canvas"></canvas>
+</template>
+
+<script>
+import MyScene from "assets/MyBabylonScene.js";
+export default {
+  name: "babylon",
+  data() {
+    return {
+      scene
+    }
+  },
+  mounted() {
+    var canvas = document.getElementById("canvas")
+    this.scene = new MyScene(canvas);
+    this.scene.doAction()
+  },
+}
