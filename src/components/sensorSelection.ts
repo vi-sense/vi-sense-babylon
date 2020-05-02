e95c2ce130b6f1629b80e6a8eed20050c84bb4d0
Lennard Grimm <lennard@netinn.de>
2020-05-02 18:26:45 +0200

    updated labels to display fetched api data

--- src/components/sensorSelection.ts
+++ src/components/sensorSelection.ts
@@ -2,99 +2,111 @@ import * as BABYLON from 'babylonjs'
 import * as GUI from "babylonjs-gui";
 
 
-export default function sensorSelectionScript(scene: BABYLON.Scene, modelMeshes) {
+async function getModel(id: number) {
+  let response = await fetch("http://visense.f4.htw-berlin.de:8080/models/" + id);
+  let body = await response.json();
+  return body;
+}
 
-    let sensors = ['node358', 'node10'];
+export default async function sensorSelectionScript(scene: BABYLON.Scene, modelMeshes) {
 
-    let container = {
-        meshes: modelMeshes
-    }
+  let model = await getModel(1);
+  let sensors = model.Sensors;
+  console.log(sensors)
+  let labels = [];
 
-    for (var i = 0; i < container.meshes.length; i++) {
-        if (sensors.includes(container.meshes[i].name)) {
-          // found sensor
-          var mat: BABYLON.PBRMaterial;
-          mat = container.meshes[i].material;
-          console.log(mat);
-          mat.albedoColor = BABYLON.Color3.Purple();
+  for (let i = 0; i < sensors.length; i++) {
+    let mesh = scene.getMeshByName(sensors[i].MeshID);
+    var mat: BABYLON.PBRMaterial;
+    mat = mesh.material;
+    mat.albedoColor = BABYLON.Color3.Purple();
 
-          var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-          var rect = new GUI.Rectangle();
-          rect.width = "70px";
-          rect.height = "70px";
-          rect.thickness = 0;
-          advancedTexture.addControl(rect);
+    var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    var rect = new GUI.Rectangle();
+    //rect.widthInPixels = 220;
+    //rect.heightInPixels = 100;
+    rect.background = "white";
+    rect.color = "black";
+    rect.alpha = 0.9;
+    rect.thickness = 0;
+    rect.adaptHeightToChildren = true;
+    rect.adaptWidthToChildren = true;
+    advancedTexture.addControl(rect);
 
-          var circle = new GUI.Ellipse();
-          circle.width = "70px";
-          circle.height = "70px";
-          circle.color = "white";
-          circle.background = "white"
-          circle.thickness = 10;
-          rect.addControl(circle);
+    var circle = new GUI.Ellipse();
+    circle.width = "70px";
+    circle.height = "70px";
+    circle.color = "white";
+    circle.background = "white";
+    circle.thickness = 10;
+    circle.isVisible = false;
+    rect.addControl(circle);
 
-          var label = new GUI.TextBlock();
-          label.text = container.meshes[i].name;
-          rect.addControl(label);
+    var label = new GUI.TextBlock();
+    label.text = sensors[i].Name;
+    //label.textWrapping = true;
+    label.resizeToFit = true;
+    console.log(rect.widthInPixels, rect.heightInPixels)
+    label.widthInPixels = rect.widthInPixels - 10;
+    label.heightInPixels = rect.heightInPixels - 10;
+    label.onTextChangedObservable.add(function(evt, picked) {
+      console.log("EVT: ", evt.parent.widthInPixels);
+      //evt.parent.widthInPixels = evt.parent.widthInPixels + 50;
+    })
+    labels.push(label);
+    //console.log(labels);
 
-          rect.linkWithMesh(container.meshes[i]);
-          //rect.linkOffsetY = -50;
+    rect.addControl(label);
+    rect.linkWithMesh(mesh);
 
-          // MESH ACTIONS
-          container.meshes[i].actionManager = new BABYLON.ActionManager(scene);
-          /*
-          container.meshes[i].actionManager.registerAction(
-            new BABYLON.InterpolateValueAction(
-              BABYLON.ActionManager.OnPointerOverTrigger,
-              container.meshes[i].material,
-              '_albedoColor',
-              BABYLON.Color3.Black(),
-              200
-            )
-          );
+    mesh.actionManager = new BABYLON.ActionManager(scene);
 
-          container.meshes[i].actionManager.registerAction(
-            new BABYLON.InterpolateValueAction(
-              BABYLON.ActionManager.OnPointerOutTrigger,
-              container.meshes[i].material,
-              '_albedoColor',
-              container.meshes[i].material._albedoColor,
-              200
-            )
-          );
-          */
+    //change mesh color on click, change state
+    mesh.actionManager.registerAction(
+      new BABYLON.ExecuteCodeAction(
+        BABYLON.ActionManager.OnPickTrigger, async function(e) {
+          console.log("picked: ", e.source);
+          if (e.source.state === "selected") {
+            e.source.state = "";
+            e.source.material.albedoColor = BABYLON.Color3.Purple();
+          } else {
+            // API CALL GETTING SENSOR DATA
+            let response = await fetch("http://visense.f4.htw-berlin.de:8080/sensors/" + sensors[i].ID);
+            let sensorData = await response.json();
 
-          //change mesh color on click, change state
-          container.meshes[i].actionManager.registerAction(
-            new BABYLON.ExecuteCodeAction(
-              BABYLON.ActionManager.OnPickTrigger, function(e) {
-                console.log("picked: ", e.source);
-                if (e.source.state === "selected") {
-                  e.source.state = "";
-                  e.source.material.albedoColor = BABYLON.Color3.Purple();
-                } else {
-                  // TODO: API CALL FOR SENSOR INFO TO DISPLAY
-                  e.source.state = "selected";
-                  e.source.material.albedoColor = BABYLON.Color3.Teal();
-                }
-              }));
+            console.log("sensorData: ", sensorData);
+            console.log("label: ", labels[i]);
+            // UPDATE LABEL TEXT
+            let text = "Name: " + sensorData.Name + "\nDescription: " + sensorData.Description + "\nValue: " + sensorData.Data[sensorData.Data.length - 1].Value.toString() + sensorData.MeasurementUnit;
+            labels[i].text = text;
+            e.source.state = "selected";
+            e.source.material.albedoColor = BABYLON.Color3.Teal();
+          }
+        }));
 
-          // change color on hover
-          container.meshes[i].actionManager.registerAction(
-            new BABYLON.ExecuteCodeAction(
-              BABYLON.ActionManager.OnPointerOverTrigger, function(e) {
-                e.source.material.albedoColor = BABYLON.Color3.Teal();
-              }));
+    // hover actions, change color
+    mesh.actionManager.registerAction(
+      new BABYLON.InterpolateValueAction(
+        BABYLON.ActionManager.OnPointerOverTrigger,
+        mesh.material,
+        'albedoColor',
+        BABYLON.Color3.Teal(),
+        200
+      ));
 
-          // revert color on hover leave
-          container.meshes[i].actionManager.registerAction(
-            new BABYLON.ExecuteCodeAction(
-              BABYLON.ActionManager.OnPointerOutTrigger, function(e) {
-                if (e.source.state != 'selected') {
-                  e.source.material.albedoColor = BABYLON.Color3.Purple();
-                }
-              }));
-
-        }
-    }
+    mesh.actionManager.registerAction(
+      new BABYLON.InterpolateValueAction(
+        BABYLON.ActionManager.OnPointerOutTrigger,
+        mesh.material,
+        'albedoColor',
+        BABYLON.Color3.Purple(),
+        200,
+        new BABYLON.PredicateCondition(
+          mesh.actionManager,
+          function() {
+            return mesh.state !== "selected";
+          }
+        )
+      ));
+  }
 }
