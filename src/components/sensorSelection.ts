db3b4b8a03163c179c1643eb77060fc20b2e07f6
Lennard Grimm <lennard@netinn.de>
2020-05-02 21:17:12 +0200

    clicking on labels changes selected state as well

--- src/components/sensorSelection.ts
+++ src/components/sensorSelection.ts
@@ -9,18 +9,25 @@ async function getModel(id: number) {
 }
 
 export default async function sensorSelectionScript(scene: BABYLON.Scene, modelMeshes) {
-
+  // GET MODEL
   let model = await getModel(1);
   let sensors = model.Sensors;
   console.log(sensors)
+
   let labels = [];
 
   for (let i = 0; i < sensors.length; i++) {
+    // CURRENT MESH
     let mesh = scene.getMeshByName(sensors[i].MeshID);
     var mat: BABYLON.PBRMaterial;
     mat = mesh.material;
     mat.albedoColor = BABYLON.Color3.Purple();
 
+    // GET SENSORDATA
+    let response = await fetch("http://visense.f4.htw-berlin.de:8080/sensors/" + sensors[i].ID);
+    let sensorData = await response.json();
+
+    // GUI SETUP
     var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
     var rect = new GUI.Rectangle();
     //rect.widthInPixels = 220;
@@ -31,6 +38,7 @@ export default async function sensorSelectionScript(scene: BABYLON.Scene, modelM
     rect.thickness = 0;
     rect.adaptHeightToChildren = true;
     rect.adaptWidthToChildren = true;
+    rect.isPointerBlocker = true;
     advancedTexture.addControl(rect);
 
     var circle = new GUI.Ellipse();
@@ -46,22 +54,37 @@ export default async function sensorSelectionScript(scene: BABYLON.Scene, modelM
     label.text = sensors[i].Name;
     //label.textWrapping = true;
     label.resizeToFit = true;
-    console.log(rect.widthInPixels, rect.heightInPixels)
+    label.paddingRightInPixels = 20;
+    label.paddingLeftInPixels = 20;
     label.widthInPixels = rect.widthInPixels - 10;
     label.heightInPixels = rect.heightInPixels - 10;
     label.onTextChangedObservable.add(function(evt, picked) {
       console.log("EVT: ", evt.parent.widthInPixels);
-      //evt.parent.widthInPixels = evt.parent.widthInPixels + 50;
+      evt.parent.widthInPixels = evt.parent.widthInPixels + 50;
     })
     labels.push(label);
-    //console.log(labels);
+
+    rect.onPointerDownObservable.add(function(e, p) {
+      // select mesh
+      console.log(e, p.target)
+      console.log(mesh);
+      if(mesh.state == "") {
+        mesh.material.albedoColor = BABYLON.Color3.Teal();
+        mesh.state = "selected";
+        p.target.text = "Name: " + sensorData.Name + "\nDescription: " + sensorData.Description + "\nValue: " + sensorData.Data[sensorData.Data.length - 1].Value.toString() + sensorData.MeasurementUnit;
+      } else {
+        mesh.material.albedoColor = BABYLON.Color3.Purple();
+        mesh.state = ""
+        p.target.text = sensorData.Name
+      }
+    })
 
     rect.addControl(label);
     rect.linkWithMesh(mesh);
 
+    // REGISTER MESH ACTIONS
     mesh.actionManager = new BABYLON.ActionManager(scene);
-
-    //change mesh color on click, change state
+    // change mesh color on click, change state
     mesh.actionManager.registerAction(
       new BABYLON.ExecuteCodeAction(
         BABYLON.ActionManager.OnPickTrigger, async function(e) {
@@ -69,6 +92,7 @@ export default async function sensorSelectionScript(scene: BABYLON.Scene, modelM
           if (e.source.state === "selected") {
             e.source.state = "";
             e.source.material.albedoColor = BABYLON.Color3.Purple();
+            labels[i].text = sensorData.Name
           } else {
             // API CALL GETTING SENSOR DATA
             let response = await fetch("http://visense.f4.htw-berlin.de:8080/sensors/" + sensors[i].ID);
